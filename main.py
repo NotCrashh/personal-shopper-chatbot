02265a8f097dc5c12086e52e70225e674f6b6b5a
gust1001 <gust1001@d.umn.edu>
2026-09-13 22:23:50 -0500

    Made it possible to ask gemini questions

--- main.py
+++ main.py
@@ -2,12 +2,34 @@ from dotenv import load_dotenv
 from google import genai
 import os
 
-load_dotenv()  # reads .env into the environment
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+def main():
+    load_dotenv()  # reads .env into the environment
 
-interaction = client.interactions.create(
-    model="gemini-3.8-flash",
-    input="Explain how AI works in a few words"
-)
-print(interaction.output_text)
+    gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+    if not gemini_api_key:
+        print("GEMINI API KEY NOT FOUND")
+        exit()
+
+    client = genai.Client(api_key=gemini_api_key)
+
+    print("Welcome to your new personal shopper bot!")
+    while True:
+        try:
+            query = input("Enter request: ")
+            if query.lower() == 'exit':
+                print("Thanks. Have a great day!")
+                exit()
+            else:
+                interaction = client.interactions.create(
+                    model="gemini-3.8-flash",
+                    input=query
+                )
+                print(interaction.output_text)
+        except Exception as e:
+            print(f"\nAn error occurred: {str(e)}")
+            print("Please try again.")
+
+if __name__ == "__main__":
+    main()
